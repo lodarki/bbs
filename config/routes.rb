42b69505fac122af16f7d246c6f0b1b754643ffc
lodarki <peter@51shepherd.com>
2013-10-29 23:27:38 +0800

    application.html.slim and _user_nav.html.slim modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bbs::Application.routes.draw do
   get "home/index"
-  devise_for :users
+  devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root :to => "home#index"
