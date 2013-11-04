351bbb7bc0d043fadc43242a54b53a002153cafe
lodarki <peter@51shepherd.com>
2013-11-04 20:01:16 +0800

    show content

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ActiveRecord::Base
   has_many :comments, :order => 'created_at DESC', :dependent => :destroy
   belongs_to :topic
   belongs_to :user
+
+  def format_detail
+  	self.detail.to_s.gsub!("\n", "<br/>")
+  	ap detail
+  end
 end
